b2378146c60edcde0a6a3ddd6db6953a74d1cc3b
vj1007 <66348884+vj1007@users.noreply.github.com>
2021-05-10 19:47:46 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace Bank
     {
         static void Main(string[] args)
         {
+            Ioperations accCurr = new Current();
+            Ioperations accSav = new Savings();
+            Ioperations accRecur = new Recurring();
+            accCurr.Deposit();
+            accCurr.Withdraw();
+            accCurr.Balance();
+            accSav.Deposit();
+            accSav.Withdraw();
+            accSav.Balance();
+            accRecur.Deposit();
+            accRecur.Withdraw();
+            accRecur.Balance();
             Console.WriteLine("Hello World!");
         }
     }
